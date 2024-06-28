2b81a4de9ebf5d36971710289ac31025722ac483
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-06-28 17:12:59 +0300

    Packages duzenlemesi yapildi

--- src/main/java/org/yenicilh/routingdatasourceproject/repository/UserRepository.java
+++ src/main/java/org/yenicilh/routingdatasourceproject/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package org.yenicilh.routingdatasourceproject.config.repository;
+package org.yenicilh.routingdatasourceproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/org/yenicilh/routingdatasourceproject/service/UserService.java
+++ src/main/java/org/yenicilh/routingdatasourceproject/service/UserService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.yenicilh.routingdatasourceproject.config.ClientDatabase;
 import org.yenicilh.routingdatasourceproject.config.ClientDatabaseContextHolder;
-import org.yenicilh.routingdatasourceproject.config.repository.UserRepository;
+import org.yenicilh.routingdatasourceproject.repository.UserRepository;
 import org.yenicilh.routingdatasourceproject.entity.UserEntity;
 
 import java.util.List;
