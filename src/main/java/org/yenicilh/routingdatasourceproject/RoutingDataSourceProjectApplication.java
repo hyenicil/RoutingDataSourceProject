c83072ae3f0d890f144e99160fd9d914f214db74
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-06-28 15:49:39 +0300

    V1.0.0

---
+++ src/main/java/org/yenicilh/routingdatasourceproject/RoutingDataSourceProjectApplication.java
@@ -0,0 +1,13 @@
+package org.yenicilh.routingdatasourceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingDataSourceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingDataSourceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yenicilh/routingdatasourceproject/RoutingDataSourceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yenicilh.routingdatasourceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingDataSourceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
